fb2315c96fbb8ce675f541d74909e35c9f369d55
Wellington Flores <wellingtonluiscardoso@hotmail.com>
2025-04-24 11:53:12 -0300

    removendo tag <a></a> e adicionando <Link> do react router

--- src/components/commom/FormLink.jsx
+++ src/components/commom/FormLink.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function FormLink({ questao, linkTexto, href }) {
   return (
     <p className="text-center">
       {questao}{" "}
-      <a
-        href={href}
+      <Link
+        to={href}
         className="text-sky-500 hover:text-sky-600 transition-colors"
       >
         {linkTexto}
-      </a>
+      </Link>
     </p>
   );
 }
